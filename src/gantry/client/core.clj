8f8423ca17b420a22410d20833fcd466ffe1fa59
Chris Targett <chris@xlevus.net>
2016-02-04 17:30:31 +0000

    stuff

--- src/gantry/client/core.clj
+++ src/gantry/client/core.clj
@@ -1,17 +1,26 @@
 (ns gantry.client.core
   (:require [clojure.edn :as edn]
+            [taoensso.timbre :refer (debug spy)]
             [zeromq.zmq :as zmq]))
 
 
 (defn listen [context]
-  (with-open [receiver (doto
-                         (zmq/socket context :pull)
-                         (zmq/connect "tcp://127.0.0.1:5555"))]
+  (with-open [receiver (doto (zmq/socket context :pull)
+                             (zmq/connect "tcp://127.0.0.1:5555"))
+              sender (doto (zmq/socket context :push)
+                           (zmq/connect "tcp://127.0.0.1:5554"))]
     (println "Listening...")
 
     (while (not (.. Thread currentThread isInterrupted))
       (let [request (zmq/receive-str receiver)]
-        (println request)))))
+
+
+        (spy :debug (zmq/send-str sender (pr-str {:id (:id request)
+                                      :response {:status 200
+                                                 :content-type "text/plain"
+                                                 :body "Hello, world"}})))
+        (debug "Request: " request)
+        ))))
 
 
 (defn -main [& args]
--- src/gantry/server/zmq.clj
+++ src/gantry/server/zmq.clj
@@ -1,40 +1,44 @@
 (ns gantry.server.zmq
-  (:require [zeromq.zmq :as zmq]
+  (:require [clojure.core.async :refer [go <!!]]
+            [clojure.edn :as edn]
+            [zeromq.zmq :as zmq]
             [taoensso.timbre :refer (info spy)]
-            [clojure.core.async :refer [go <!!]]
             [gantry.server.channels :as channels]))
 
 
-(defonce context (zmq/context 1))
-(defonce sockets (atom {}))
+(let [context (zmq/context 1)]
+
+  (defn pull-requests
+    "Pulls requests off a channel and pushes them to zmq socket"
+    [chan]
+    (info "pull-requests")
+
+    (go (let [request-socket (doto (zmq/socket context :push)
+                                   (zmq/bind "tcp://*:5555"))]
+          (while true
+            (let [request (<!! chan)]
+              (info "Request!" (:id request))
+
+              (zmq/send-str request-socket (pr-str request))
+
+              ;(channels/new-response (:id request)
+              ;                       {:status 200
+              ;                        :content-type "text/plain"
+              ;                        :body "Hello, world"})
+              )))))
 
 
-(defn get-conn
-  "Gets push socket for ZMQ"
-  [id]
-  (if (get @sockets id) nil
-    (let [new-sock (doto (zmq/socket context :push)
-                         (zmq/bind "tcp://*:5555"))]
-      (info "Binding new zmq sock:" id)
-      (swap! sockets assoc id new-sock)))
-  (get @sockets id))
+  (defn pull-responses []
+    (info "pull-responses")
+    (go (let [response-socket (doto (zmq/socket context :pull)
+                                    (zmq/bind "tcp://*:5554"))]
 
+          (while (not (.. Thread currentThread isInterrupted))
+            (let [response (edn/read (zmq/receive-str response-socket))]
+              (spy :debug response)
+              (channels/new-response (:id response) (:response response)))))))
 
-(defn pull-requests
-  "Pulls requests off a channel and pushes them to zmq socket"
-  [chan]
-
-  (go (while true
-        (let [req (<!! chan)]
-          (info "Request!" (:id req))
-
-          ; hur hur, lets do some work
-          (Thread/sleep 1000)
-
-          (channels/new-response (:id req)
-                                 {:status 200
-                                  :content-type "text/plain"
-                                  :body "Hello, world"})
-
-          ))))
-
+  (defn start []
+     (pull-requests channels/incoming-requests)
+     (pull-responses)
+     ))
